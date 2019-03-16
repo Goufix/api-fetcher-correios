8bbf811b0624b576b94540109c87356d6880e222
Goufix <alifer.17.11@gmail.com>
2019-03-16 10:05:59 -0300

    first

---
+++ estados.js
@@ -0,0 +1,8 @@
+const estados = [
+  {
+    PR: [{ CEP: [80020310, 83308390] }, { VALORES: [] }],
+    RS: [{ CEP: [96290970, 99885000, 99885000, 96450970] }, { VALORES: [] }]
+  }
+];
+
+module.exports = estados;
---
+++ index.js
@@ -0,0 +1,1 @@
+const estados = require("./estados");
