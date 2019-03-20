fa3327af99b80d33c6cf72497969ef4e7d0d2bb6
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2019-03-20 19:26:34 -0300

    fix: cix grammar

--- src/utils/parseCustomFloat.js
+++ src/utils/parseCustomFloat.js
@@ -1,6 +1,6 @@
 module.exports = function parseCustomFloat(value, decimalSeparator = ',') {
   if (decimalSeparator === '.') {
-    throw new Error('You should use the build in parseFloat');
+    throw new Error('You should use the built in parseFloat');
   }
 
   return parseFloat(value.replace('.', '').replace(decimalSeparator, '.'));
